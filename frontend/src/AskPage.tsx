1b6816dc4685958a56e9ab7c1790da62b37423bb
Carlos <carlosbolorino@hotmail.com>
2022-02-16 19:09:41 +0100

    forms validation

--- frontend/src/AskPage.tsx
+++ frontend/src/AskPage.tsx
@@ -1,4 +1,72 @@
 import { Page } from './Page';
+import {
+  Fieldset,
+  FieldContainer,
+  FieldLabel,
+  FieldInput,
+  FieldTextArea,
+  FormButtonContainer,
+  PrimaryButton,
+  FieldError,
+} from './Styles';
+import { useForm } from 'react-hook-form';
 
-export const AskPage = () => <Page title="Ask a question">{null}</Page>;
+type FormData = {
+  title: string;
+  content: string;
+};
+
+export const AskPage = () => {
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm<FormData>({
+    mode: 'onBlur',
+  });
+
+  const onSubmit = () => console.log('submit');
+
+  return (
+    <Page title="Ask a question">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Fieldset>
+          <FieldContainer>
+            <FieldLabel htmlFor="title">Title</FieldLabel>
+            <FieldInput
+              id="title"
+              type="text"
+              {...register('title', {
+                required: true,
+                minLength: 10,
+              })}
+            />
+            {errors.title && errors.title.type === 'required' && (
+              <FieldError>You must enter the question title</FieldError>
+            )}
+            {errors.title && errors.title.type === 'minLength' && (
+              <FieldError>The title must be at least 10 characters</FieldError>
+            )}
+            <FieldTextArea
+              id="content"
+              {...register('content', { required: true, minLength: 50 })}
+            />
+            {errors.content && errors.content.type === 'required' && (
+              <FieldError>You must enter the question content</FieldError>
+            )}
+            {errors.content && errors.content.type === 'minLength' && (
+              <FieldError>
+                The content must be at least 50 characters
+              </FieldError>
+            )}
+          </FieldContainer>
+          <FormButtonContainer>
+            <PrimaryButton type="submit">Submit Your Question</PrimaryButton>
+          </FormButtonContainer>
+        </Fieldset>
+      </form>
+    </Page>
+  );
+};
+
 export default AskPage;
--- frontend/src/QuestionPage.tsx
+++ frontend/src/QuestionPage.tsx
@@ -1,17 +1,38 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
-import { gray3, gray6 } from './Styles';
-
+import {
+  gray3,
+  gray6,
+  Fieldset,
+  FieldContainer,
+  FieldLabel,
+  FieldTextArea,
+  FormButtonContainer,
+  PrimaryButton,
+  FieldError,
+} from './Styles';
+import { useForm } from 'react-hook-form';
 import { useState, useEffect, Fragment } from 'react';
 import { Page } from './Page';
 import { useParams } from 'react-router-dom';
 import { QuestionData, getQuestion } from './QuestionsData';
 import { AnswerList } from './AnswerList';
 
+type FormData = {
+  content: string;
+};
+
 export const QuestionPage = () => {
   const [question, setQuestion] = useState<QuestionData | null>(null);
   const { questionId } = useParams();
-
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm<FormData>({
+    mode: 'onBlur',
+  });
+  const onSubmit = () => console.log('submit');
   useEffect(() => {
     const doGetQuestion = async (questionId: number) => {
       const foundQuestion = await getQuestion(questionId);
@@ -66,6 +87,38 @@ export const QuestionPage = () => {
   ${question.created.toLocaleTimeString()}`}
             </div>
             <AnswerList data={question.answers} />
+            <form
+              onSubmit={handleSubmit(onSubmit)}
+              css={css`
+                margin-top: 20px;
+              `}
+            >
+              <Fieldset>
+                <FieldContainer>
+                  <FieldLabel htmlFor="content">Your Answer</FieldLabel>
+                  <FieldTextArea
+                    id="content"
+                    {...register('content', {
+                      required: true,
+                      minLength: 50,
+                    })}
+                  />
+                  {errors.content && errors.content.type === 'required' && (
+                    <FieldError>You must enter the answer</FieldError>
+                  )}
+                  {errors.content && errors.content.type === 'minLength' && (
+                    <FieldError>
+                      The answer must be at least 50 characters
+                    </FieldError>
+                  )}
+                </FieldContainer>
+                <FormButtonContainer>
+                  <PrimaryButton type="submit">
+                    Submit Your Answer
+                  </PrimaryButton>
+                </FormButtonContainer>
+              </Fieldset>
+            </form>
           </Fragment>
         )}
       </div>

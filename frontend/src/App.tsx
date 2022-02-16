4d73e931986ebd0603ce0b0de7a2d959a5a30fd8
Carlos <carlosbolorino@hotmail.com>
2022-02-16 18:22:33 +0100

    routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,19 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
+import React from 'react';
 import { Header } from './Header';
 import { HomePage } from './HomePage';
 import { fontFamily, fontSize, gray2 } from './Styles';
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { AskPage } from './AskPage';
 import { SearchPage } from './SearchPage';
 import { SignInPage } from './SignInPage';
 import { NotFoundPage } from './NotFoundPage';
 import { QuestionPage } from './QuestionPage';
 
 function App() {
+  const AskPage = React.lazy(() => import('./AskPage'));
+
   return (
     <BrowserRouter>
       <div
@@ -25,7 +27,25 @@ function App() {
         <Routes>
           <Route path="" element={<HomePage />} />
           <Route path="search" element={<SearchPage />} />
-          <Route path="ask" element={<AskPage />} />
+          <Route
+            path="ask"
+            element={
+              <React.Suspense
+                fallback={
+                  <div
+                    css={css`
+                      margin-top: 100px;
+                      text-align: center;
+                    `}
+                  >
+                    Loading...
+                  </div>
+                }
+              >
+                <AskPage />
+              </React.Suspense>
+            }
+          />
           <Route path="signin" element={<SignInPage />} />
           <Route path="questions/:questionId" element={<QuestionPage />} />
           <Route path="*" element={<NotFoundPage />} />
--- frontend/src/AskPage.tsx
+++ frontend/src/AskPage.tsx
@@ -1,3 +1,4 @@
 import { Page } from './Page';
 
 export const AskPage = () => <Page title="Ask a question">{null}</Page>;
+export default AskPage;
